340c09a63b3edfae00050eef6e01bae3f396e316
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-09-03 18:30:11 +0530

    Added solution for testing aspnet user table

---
+++ TestIdentity/TestIdentity/Models/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestIdentity.Models
+{
+    public class Organization
+    {
+        [Key]
+        public int OrganizationId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+
+    }
+}
